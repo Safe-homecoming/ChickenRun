01b9b65308c43549fb9cb571606b353ea18f173e
YOUNGJUYOON1992 <dudwntld@gmail.com>
2019-12-02 17:12:47 +0900

    start new dd

---
+++ app/src/main/java/com/example/chickenrun/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.chickenrun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
