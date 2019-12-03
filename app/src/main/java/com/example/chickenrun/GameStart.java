e496ca851ab26eab3b623837b70763a519fee7fe
YOUNGJUYOON1992 <dudwntld@gmail.com>
2019-12-03 20:47:19 +0900

    Merge branch 'master' of https://github.com/Safe-homecoming/ChickenRun
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/chickenrun/GameStart.java
+++ app/src/main/java/com/example/chickenrun/GameStart.java
@@ -1,14 +1,65 @@
 package com.example.chickenrun;
 
 import androidx.appcompat.app.AppCompatActivity;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class GameStart extends AppCompatActivity {
+
+    Socket socket;
+
+    Button button;
+    EditText editText;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_start);
+        try {
+            socket = IO.socket("http://ec2-13-125-121-5.ap-northeast-2.compute.amazonaws.com:3000");
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                socket.emit("message_from_client", "Hi~ 나는 안드로이드야.");
+            }
+        }).on("message_from_server", new Emitter.Listener() {
+            @Override
+            public void call(final Object... args) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText(args[0].toString());
+                    }
+                });
+            }
+        });
+        socket.connect();
+
+        editText = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.result);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String msg = editText.getText().toString();
+                socket.emit("message_from_client", msg);
+            }
+        });
     }
+
+
 }
