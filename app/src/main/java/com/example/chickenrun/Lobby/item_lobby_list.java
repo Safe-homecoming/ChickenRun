cef81043cc4c5b4c01efa5b2b8fc889cc1c77cb3
hunkim <hun73877@naver.com>
2019-12-03 16:55:46 +0900

    php로 방 입장, 퇴장 처리 작업 중

--- app/src/main/java/com/example/chickenrun/Lobby/item_lobby_list.java
+++ app/src/main/java/com/example/chickenrun/Lobby/item_lobby_list.java
@@ -1,5 +1,46 @@
 package com.example.chickenrun.Lobby;
 
+import com.google.gson.annotations.SerializedName;
+
 public class item_lobby_list
 {
+
+    @SerializedName("room_index")
+    String roomIndex;
+
+    @SerializedName("room_name")
+    String roomName;
+
+    @SerializedName("user_count")
+    String roomCount;
+
+    public String getRoomIndex()
+    {
+        return roomIndex;
+    }
+
+    public void setRoomIndex(String roomIndex)
+    {
+        this.roomIndex = roomIndex;
+    }
+
+    public String getRoomName()
+    {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName)
+    {
+        this.roomName = roomName;
+    }
+
+    public String getRoomCount()
+    {
+        return roomCount;
+    }
+
+    public void setRoomCount(String roomCount)
+    {
+        this.roomCount = roomCount;
+    }
 }
--- app/src/main/java/com/example/chickenrun/gameRoom/Activity_Waiting_Room.java
+++ app/src/main/java/com/example/chickenrun/gameRoom/Activity_Waiting_Room.java
@@ -6,26 +6,48 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.chickenrun.ApiClient;
 import com.example.chickenrun.ApiInterface;
 import com.example.chickenrun.Lobby.Activity_Lobby;
 import com.example.chickenrun.Lobby.item_lobby_list;
 import com.example.chickenrun.R;
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.example.chickenrun.Lobby.Activity_Lobby.GET_MY_JOIN_INDEX;
+import static com.example.chickenrun.Lobby.Activity_Lobby.GET_ROOM_INDEX;
+import static com.example.chickenrun.Lobby.Activity_Lobby.HANDLER_DELETE;
 
 public class Activity_Waiting_Room extends AppCompatActivity
 {
@@ -40,6 +62,22 @@ public class Activity_Waiting_Room extends AppCompatActivity
 
     TextView button_waiting_ready;
 
+    // 소켓 연결 설정
+    private Socket mSocket;
+    {
+        try
+        {
+            Log.e(TAG, "instance initializer: 소켓 연결 시작" );
+            mSocket = IO.socket("http://chat.socket.io");
+            Log.e(TAG, "instance initializer: mSocket: " + mSocket);
+        }
+
+        catch (URISyntaxException e)
+        {
+            Log.e(TAG, "instance initializer: URISyntaxException: " + e.getMessage());
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -47,6 +85,10 @@ public class Activity_Waiting_Room extends AppCompatActivity
         setContentView(R.layout.activity_waiting_room);
 
         mContext = Activity_Waiting_Room.this;
+
+        // 소켓 연결
+        mSocket.on("new message", onNewMessage);
+        mSocket.connect();
 
         // 리사이클러뷰 세팅
         Participant_list = findViewById(R.id.waiting_room_participant);
@@ -66,6 +108,116 @@ public class Activity_Waiting_Room extends AppCompatActivity
 
         // todo: 참가중인 유저 불러오기
         getParticipantList();
+    }
+
+    // 메시지 전송
+    private void attemptReadyOn()
+    {
+        String message = "Ready|on";
+        if (TextUtils.isEmpty(message))
+        {
+            return;
+        }
+
+        mSocket.emit("new message", message);
+    }
+
+    private Emitter.Listener onNewMessage = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    JSONObject data = (JSONObject) args[0];
+                    Log.e(TAG, "run: data: " + data );
+                    String username;
+                    String message;
+                    try {
+                        username = data.getString("username");
+                        message = data.getString("message");
+                        Log.e(TAG, "run: username" + username );
+
+                    } catch (JSONException e) {
+                        return;
+                    }
+
+                    // 메시지 수신받기
+                    // add the message to view
+//                    addMessage(username, message);
+                }
+            });
+        }
+    };
+
+    // 소켓 연결 해제
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mSocket.disconnect();
+        mSocket.off("new message", onNewMessage);
+
+        // todo: 방 퇴장하기 (mysql)
+        disconnectRoom(GET_ROOM_INDEX, GET_MY_JOIN_INDEX);
+    }
+
+    // todo: 방 퇴장하기 (mysql)
+    public void disconnectRoom(final String roomIndex, final String myJoinIndex)
+    {
+        Log.e(TAG, "disconnectRoom: 방 퇴장 처리하기" );
+
+        StringRequest stringRequest
+                = new StringRequest(Request.Method.POST,
+                "http://ec2-13-125-121-5.ap-northeast-2.compute.amazonaws.com/chicken/outRoom.php",
+                new com.android.volley.Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response)
+                    {
+                        Log.e(TAG, "addPaymentHistory onResponse: " + response.trim());
+
+                        // 방 퇴장 처리 완료 메시지
+                        if (response.trim().equals("success_room_delete"))
+                        {
+                            Log.e(TAG, "createRoom: 방 퇴장 처리 완료" );
+
+                            // 방 삭제되면 핸들러로 Activity_Lobby로 삭제 알림 보내주기 (방 목록 새로고침)
+                            Message hdmg = HANDLER_DELETE.obtainMessage();
+                            Log.e(TAG, "ReceiveThread: hdmg = msgHandler.obtainMessage(): " + hdmg);
+                            hdmg.what = 1111;
+                            hdmg.obj = "refreshList_deleteRoom";
+                            HANDLER_DELETE.sendMessage(hdmg);
+
+                        }
+                    }
+                },
+                new com.android.volley.Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        Log.e("VolleyError", "에러: " + error.toString());
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError
+            {
+                Map<String, String> params = new HashMap<>();
+
+                params.put("roomIndex", roomIndex);
+                params.put("myJoinIndex", myJoinIndex);
+
+                return params;
+            }
+        };
+
+        // requestQueue로 로그인 결과값 요청을 시작한다.
+        RequestQueue requestQueue = Volley.newRequestQueue(mContext);
+
+        // stringRequest메소드에 기록한 내용들로 requestQueue를 시작한다.
+        requestQueue.add(stringRequest);
+
     }
 
     // todo: 참가중인 유저 불러오기
@@ -91,7 +243,6 @@ public class Activity_Waiting_Room extends AppCompatActivity
             public void onResponse(Call<List<item_participant_user>> call, Response<List<item_participant_user>> response)
             {
                 itemParticipant = response.body();
-
 /*
                 for (int i = 0; i < itemParticipant.size(); i++)
                 {
@@ -106,7 +257,7 @@ public class Activity_Waiting_Room extends AppCompatActivity
             @Override
             public void onFailure(Call<List<item_participant_user>> call, Throwable t)
             {
-                Log.e(TAG, "onFailure: t: " + t.getMessage() );
+                Log.e(TAG, "onFailure: t: " + t.getMessage());
             }
         });
     }
