7459bf9c7aa3a5862183ccd9f28d788d82e5c605
hunkim <hun73877@naver.com>
2019-12-05 03:10:14 +0900

    유저 입장, 레디, 게임 시작 동기화 완료.

--- app/src/main/java/com/example/chickenrun/gameRoom/adapterWaitingRoom.java
+++ app/src/main/java/com/example/chickenrun/gameRoom/adapterWaitingRoom.java
@@ -10,11 +10,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import static com.example.chickenrun.gameRoom.Activity_Waiting_Room.GET_HEIGHT_USER_SPACE;
 import static com.example.chickenrun.gameRoom.Activity_Waiting_Room.HANDLER_READY_CLICK;
 
 import com.example.chickenrun.R;
@@ -59,17 +61,18 @@ public class adapterWaitingRoom extends RecyclerView.Adapter<adapterWaitingRoom.
         ((Activity) holder.player_image.getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         int deviceWidth = displayMetrics.widthPixels;  // 핸드폰의 가로 해상도를 구함.
 //            int deviceHeight = displayMetrics.heightPixels;  // 핸드폰의 세로 해상도를 구함.
-        Log.e(TAG, "onBindViewHolder: deviceWidth    : " + deviceWidth);
+//        Log.e(TAG, "onBindViewHolder: deviceWidth    : " + deviceWidth);
 
         deviceWidth = deviceWidth / 2;
-        Log.e(TAG, "onBindViewHolder: deviceWidth / 2: " + deviceWidth);
+//        Log.e(TAG, "onBindViewHolder: deviceWidth / 2: " + deviceWidth);
 
-        int deviceHeight = (int) ((float) deviceWidth * 0.95);  // 세로의 길이를 가로의 길이의 1배로 or 1.5 = 1.5배로
+        int deviceHeight = (int) ((float) deviceWidth * 0.94);  // 세로의 길이를 가로의 길이의 1배로 or 1.5 = 1.5배로
 //            int deviceHeight = deviceWidth * 1.1;  // 세로의 길이를 가로의 길이의 1배로 or 1.5 = 1.5배로
 
-        holder.player_image.getLayoutParams().width = deviceWidth;  // 아이템 뷰의 세로 길이를 구한 길이로 변경
-        holder.player_image.getLayoutParams().height = deviceHeight;  // 아이템 뷰의 세로 길이를 구한 길이로 변경
-        holder.player_image.requestLayout(); // 변경 사항 적용
+        holder.view_size.getLayoutParams().width = deviceWidth;  // 아이템 뷰의 세로 길이를 구한 길이로 변경
+        holder.view_size.getLayoutParams().height = GET_HEIGHT_USER_SPACE / 2;  // 아이템 뷰의 세로 길이를 구한 길이로 변경
+        holder.view_size.requestLayout(); // 변경 사항 적용
+        Log.e(TAG, "onBindViewHolder: GET_HEIGHT_USER_SPACE: " + GET_HEIGHT_USER_SPACE / 2 );
 
         /**
          이렇게 하면 동적으로 원하는 크기의 아이템 항목을 구성할 수 있고,
@@ -95,8 +98,18 @@ public class adapterWaitingRoom extends RecyclerView.Adapter<adapterWaitingRoom.
             holder.host_mark.setVisibility(View.GONE);
         }
 
+        // 레디 표시
+        if (itemParticipant.get(position).isReady)
+        {
+            holder.button_ready.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            holder.button_ready.setVisibility(View.GONE);
+        }
+
         // 레디 클릭 알림 수신 대기 (핸들러)
-        HANDLER_READY_CLICK = new Handler()
+/*        HANDLER_READY_CLICK = new Handler()
         {
             @Override
             public void handleMessage(Message msg)
@@ -115,15 +128,18 @@ public class adapterWaitingRoom extends RecyclerView.Adapter<adapterWaitingRoom.
 
                     for (int i = 0; i < itemParticipant.size(); i++)
                     {
-                        if (itemParticipant.get(position).getUserName().equals(GET_MY_NAME))
+                        Log.e(TAG, "handleMessage: itemParticipant.get(position).getUserName(): " + itemParticipant.get(position).getUserName() );
+                        if (itemParticipant.get(i).getUserName().equals(readyReceive[1]))
                         {
+                            Log.e(TAG, "handleMessage: 도달함: itemParticipant.get(position).getUserName().equals(GET_MY_NAME)" );
+
                             if (readyReceive[0].equals("ready_off_recycler_view"))
                             {
                                 Log.e(TAG, "handleMessage: " + readyReceive[1] + "유저 ready Off");
                                 holder.button_ready.setVisibility(View.GONE);
                             }
 
-                            else/* if (readyReceive[0].equals("ready_on_recycler_view"))*/
+                            else*//* if (readyReceive[0].equals("ready_on_recycler_view"))*//*
                             {
                                 Log.e(TAG, "handleMessage: " + readyReceive[1] + "유저 ready On");
                                 holder.button_ready.setVisibility(View.VISIBLE);
@@ -132,7 +148,7 @@ public class adapterWaitingRoom extends RecyclerView.Adapter<adapterWaitingRoom.
                     }
                 }
             }
-        };
+        };*/
 
         // 준비 버튼 클릭
     }
@@ -150,7 +166,7 @@ public class adapterWaitingRoom extends RecyclerView.Adapter<adapterWaitingRoom.
         public TextView host_mark;
         public TextView button_ready;
         public ImageView player_image;
-
+        public LinearLayout view_size;
 
         public ViewHolder(@NonNull View itemView)
         {
@@ -162,6 +178,7 @@ public class adapterWaitingRoom extends RecyclerView.Adapter<adapterWaitingRoom.
             host_mark = itemView.findViewById(R.id.host_mark);
             button_ready = itemView.findViewById(R.id.button_ready);
             player_image = itemView.findViewById(R.id.player_image);
+            view_size = itemView.findViewById(R.id.view_size);
         }
     }
 }
